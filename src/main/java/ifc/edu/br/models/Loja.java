0b92abe1c96e952266a4c0e75701c747244958a6
Nickanzo <nicoeanzola@gmail.com>
2022-07-13 21:52:44 -0300

    Criando relação entre Funcionario/Loja

--- src/main/java/ifc/edu/br/models/Loja.java
+++ src/main/java/ifc/edu/br/models/Loja.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 
@@ -19,6 +20,8 @@ public class Loja {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "loja_seq")
     @SequenceGenerator(name = "loja_seq", initialValue = 1)
     private Long id;
+    @ManyToOne
+    private Funcionario funcionario;    
     private String nome;
     private String cnpj;
     private String cep;
@@ -26,7 +29,8 @@ public class Loja {
     public Loja() {
     }
 
-    public Loja(String nome, String cnpj, String cep) {
+    public Loja(Funcionario funcionario, String nome, String cnpj, String cep) {
+        this.funcionario = funcionario;
         this.nome = nome;
         this.cnpj = cnpj;
         this.cep = cep;
@@ -63,6 +67,12 @@ public class Loja {
     public void setId(Long id) {
         this.id = id;
     }
-        
     
+    public Funcionario getFuncionario() {
+        return funcionario;
+    }
+
+    public void setFuncionario(Funcionario funcionario) {
+        this.funcionario = funcionario;
+    }            
 }
